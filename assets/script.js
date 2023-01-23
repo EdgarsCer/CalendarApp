6f2bdea419941f908ab4bed471f71bead910bdaa
EdgarsCer <Edgarscernijs1993@gmail.com>
2023-01-23 21:28:05 +0000

    Button now saves and overwrites inputvalues and also code commented

--- assets/script.js
+++ assets/script.js
@@ -5,28 +5,24 @@ setInterval(
     }, 1000);
 
 let inputFields = document.querySelectorAll(".input-field");
-let inputValues = {
-    values: Array(inputFields.length).fill("")
-};
 
-if (inputFields.length) { // check for input fields' length instead of existence
-    inputFields.forEach(function (inputFields, index) {
-        let storedText = localStorage.getItem("inputValues" + index);
-        if (storedText) {
-            inputFields.value = storedText;
+if (inputFields.length) { // check if there are any input fields at all
+    inputFields.forEach(function (inputField, index) { // loop through each input field
+        let storedText = localStorage.getItem("inputValues" + index); // get the stored text for this input field to display
+        if (storedText) { // checks if there is stored text equals = TRUE 
+            inputField.value = storedText; // set the input field's value to what was stored in local
         }
     });
-
-    // inputFields.forEach(function (inputFields) {
-    //     inputFields.addEventListener("focus", function () {
-    //         // inputFields.value = "";
-    //     });
-    // });
-
-    inputFields.forEach(function (inputFields, index) {
-        inputFields.addEventListener("blur", function () {
-            let inputValue = inputFields.value; 
-            localStorage.setItem("inputValues" + index, inputValue);
-        });
-    });
 }
+// Select all elements with class 'save' and store them in a variable to use latter
+let saveButtons = document.querySelectorAll(".save");
+
+// go through over each save button and add a click event listener
+saveButtons.forEach(function (saveBtn, index) {
+saveBtn.addEventListener("click", function () {
+// Get the value of the corresponding input field
+let inputValue = inputFields[index].value;
+// Store the input value in local storage using the index as the key
+localStorage.setItem("inputValues" + index, inputValue);
+});
+});
